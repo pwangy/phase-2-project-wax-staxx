c81ae94e3f95b8efa34ddc854305bcecc0da517f
Vincent Revard <Vincent.Revard@gmail.com>
2024-03-02 18:24:59 -0800

    Add custom hook useErrorAlerts.ErrorPage.jsx

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,20 +1,12 @@
-// import { useState } from "react"
+import { useState } from "react"
 
 export const useErrorAlerts = () => {
-    // const [error, setError] = useState('')
-    // const includeErrorAlerts = (err) => {
-    //     setError(`Re-attempt Action: Process Failed.\nIssue: ${err.message}`)
-    //     setTimeout(() => setError(''), 5000)
-    //     }
-    //     return [error, includeErrorAlerts]
+    const [error, setError] = useState('')
+    const includeErrorAlerts = (err) => {
+        setError(`Re-attempt Action: Process Failed.\nIssue: ${err.message}`)
+        setTimeout(() => setError(''), 5000)
+        }
+        return [error, includeErrorAlerts]
 };
 
 export default useErrorAlerts
-
-// import { useRouteError } from 'react-router-dom'
-
-// const ErrorPage = () => {
-//     const error = useRouteError()
-//     return (
-//         <div className='err'>{error.error.message}</div>
-// )}
