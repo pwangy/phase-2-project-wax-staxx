615b167e430a6293dc3b12540177eabfe291d321
Peggy <blipsandclicks@gmail.com>
2024-03-05 16:27:59 -0800

    Revert "Revert "Add to and del fr staxx""

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
-import App from './App'
-import LibraryContainer from './pages/LibraryContainer'
-// import MyStaxx from './pages/MyStaxx'
-import Form from './pages/Form'
-import ErrorPage from './pages/ErrorPage'
+import App from '../App'
+import ErrorPage from '../pages/ErrorPage'
+import LibraryContainer from '../pages/LibraryContainer'
+import Form from '../pages/Form'
 
 // ! Outlets: App
 const router = createBrowserRouter([
