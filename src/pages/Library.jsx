374c3cacf327dd22ab65b79e48875cfe9eead7cf
Vincent Revard <Vincent.Revard@gmail.com>
2024-03-07 12:10:03 -0800

    Add useMemo Library

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,32 +1,33 @@
+import { useMemo } from 'react'
 import Card from '../components/Card'
 
 const Library = ({ showStaxx, albums, searchQuery, sortSelected }) => {
-    const filterAndSortedAlbums = (albums, searchQuery, sortSelected, filterPageCondition) => {
+    const filterAndSortedAlbums = useMemo(() => (albums, searchQuery, sortSelected, filterPageCondition) => {
         return albums
-        .filter((album) => {
-            if (!album.artist || !album.title) return false
-            return (
-            album.artist.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            album.title.toLowerCase().includes(searchQuery.toLowerCase())
-            )
-        })
-        .filter(filterPageCondition)
-        .sort((a, b) => {
-            if (sortSelected !== 'All') {
-            return a[sortSelected].toLowerCase().localeCompare(b[sortSelected].toLowerCase())
-            } else {
-            return true
-            }
-        })
-        .map((a) => (
-            <Card
-            key={a.id}
-            {...a}
-            showStaxx={showStaxx}
-            isAlbumInCollection={() => a.inCollection}
-            />
-        ))
-    }
+            .filter((album) => {
+                if (!album.artist || !album.title) return false
+                return (
+                    album.artist.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                    album.title.toLowerCase().includes(searchQuery.toLowerCase())
+                )
+            })
+            .filter(filterPageCondition)
+            .sort((a, b) => {
+                if (sortSelected !== 'All') {
+                    return a[sortSelected].toLowerCase().localeCompare(b[sortSelected].toLowerCase())
+                } else {
+                    return true
+                }
+            })
+            .map((a) => (
+                <Card
+                    key={a.id}
+                    {...a}
+                    showStaxx={showStaxx}
+                    isAlbumInCollection={() => a.inCollection}
+                />
+            ))
+    }, [showStaxx])
 
     const renderLibrary = filterAndSortedAlbums(albums, searchQuery, sortSelected, () => true)
     const renderStaxx = filterAndSortedAlbums(
@@ -35,7 +36,6 @@ const Library = ({ showStaxx, albums, searchQuery, sortSelected }) => {
         sortSelected,
         (album) => album.inCollection === true
     )
-
     return (
         <article className='article-wrapper'>
         <div className='article-title'>
